d3b8b9c1a8fc35092422aafe2cfed87dd3e2d7d4
Alex Chojnacki <thealex@umich.edu>
2018-03-19 14:54:31 -0400

    Factored sidebar into it's own small component

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,12 +1,6 @@
 import React, { Component } from "react";
-import {
-  Title,
-  Sidebar as GrommetSidebar,
-  Header,
-  Menu,
-  Box,
-  Anchor
-} from "grommet";
+import { Title, Sidebar as GrommetSidebar, Header, Box } from "grommet";
+import SidebarMenu from "./SidebarMenu";
 
 class Sidebar extends Component {
   constructor(props) {
@@ -22,11 +16,7 @@ class Sidebar extends Component {
           <Title>MailAppTitle</Title>
         </Header>
         <Box flex="grow" justify="start">
-          <Menu primary={true}>
-            <Anchor href="#">Dashboard</Anchor>
-            <Anchor href="#">Profile</Anchor>
-            <Anchor href="#">New</Anchor>
-          </Menu>
+          <SidebarMenu />
         </Box>
       </GrommetSidebar>
     );
