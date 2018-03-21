fcc84edbad60517f1a91b5892400396bb244b032
Alex Chojnacki <thealex@umich.edu>
2018-03-21 15:05:19 -0400

    Added private route component

---
+++ client/src/PrivateRoute.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// Taken from https://tylermcginnis.com/react-router-protected-routes-authentication/
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      fakeAuth.isAuthenticated === true ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/login" />
+      )
+    }
+  />
+);
+
+export default PrivateRoute;
