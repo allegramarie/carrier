3bbb27d3d29675da432cfd123e40a9d9270704ed
Alex Chojnacki <thealex@umich.edu>
2018-03-21 15:18:11 -0400

    import and use client side auth

--- client/src/PrivateRoute.js
+++ client/src/PrivateRoute.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Auth from "./Auth";
 
 // Taken from https://tylermcginnis.com/react-router-protected-routes-authentication/
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
     {...rest}
     render={props =>
-      fakeAuth.isAuthenticated === true ? (
+      Auth.isAuthenticated() === true ? (
         <Component {...props} />
       ) : (
         <Redirect to="/login" />
