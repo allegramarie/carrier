2e2c4bdb1ed1d687fb180ac6a2b7b2ecfc72d6fd
Alex Chojnacki <thealex@umich.edu>
2018-03-21 16:47:29 -0400

    Added session helpers

--- auth/index.js
+++ auth/index.js
@@ -15,6 +15,23 @@ const genToken = () => {
   return "token-" + new Date().toISOString();
 };
 
+const setSession = (token, username) => {
+  session = { username };
+  sessions[token] = session;
+  return session;
+};
+
+const validateUserLogin = (username, password) => {
+  return new Promise((resolve, reject) => {
+    // Check in the database -- simulating delay
+    if (username === "alex" && password === "rawr") {
+      setTimeout(resolve, 100, true);
+    } else {
+      setTimeout(resolve, 100, false);
+    }
+  });
+};
+
 // Middleware that allows every session to access request.session
 const attachSession = (request, response, next) => {
   const { token } = request.body;
@@ -26,5 +43,7 @@ module.exports = {
   genToken,
   getSession,
   sessions,
-  attachSession
+  attachSession,
+  validateUserLogin,
+  setSession
 };
