e532f29f25dd3da01898cd75c78abc6e043c80d1
YuQing Dong <yuqingdong@YuQings-MacBook-Air.local>
2018-03-27 12:21:35 -0400

    loading bug

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,13 +2,11 @@ import { combineReducers } from "redux";
 import campaigns from "./campaigns";
 import contacts from "./contacts";
 import user from "./user";
-import error from "./error";
 
 const rootReducer = combineReducers({
   user,
   campaigns,
-  contacts,
-  error
+  contacts
 });
 
 export default rootReducer;
