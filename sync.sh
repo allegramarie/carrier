773734daebfcd252f10acd38325074bdf7bb6f37
Alex Chojnacki <thealex@umich.edu>
2018-04-06 11:25:23 -0400

    Improved sync script for deployment

--- sync.sh
+++ sync.sh
@@ -1,3 +1,6 @@
+cd client
+yarn run build
+cd ..
 rm -rf client/node_modules
 scp -i hrnyc13-targaryan.pem -r `ls -1a | grep -E -v '(node_modules|\.git|^\.|^\.\.)'` ec2-user@ec2-54-227-125-175.compute-1.amazonaws.com:app/
 cd client
